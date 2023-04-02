2510053cf7531975d55bbdf7f78571bc08425a42
Anael Medeiros <anaelj@gmail.com>
2023-04-02 08:49:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cron = require("node-cron");
 
 // Define o repositório e o intervalo de atualização
 const repo = simpleGit("git@github.com:anaelj/autoupdate.git");
-const intervaloAtualizacao = "*/1 * * * *"; // atualiza a cada 10 minutos
+const intervaloAtualizacao = "*/1 * * * *"; // atualiza a cada 1 minutos
 
 // Define a tarefa de atualização
 const tarefaAtualizacao = cron.schedule(intervaloAtualizacao, () => {
