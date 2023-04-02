f101ffb0cf381c77a017744470c95a626070090a
Anael Medeiros <anaelj@gmail.com>
2023-04-02 09:42:24 -0400

    Update autoUpdate.js

--- autoUpdate.js
+++ autoUpdate.js
@@ -10,7 +10,7 @@ const options = {
 
 // Define o repositório e o intervalo de atualização
 const repo = simpleGit("https://github.com/anaelj/autoupdate", options);
-const intervaloAtualizacao = "*/1 * * * *"; // atualiza a cada 1 minuto
+const intervaloAtualizacao = "*/1 * * * *"; // atualiza a cada 2 minutos
 
 // Define a tarefa de atualização
 export const tarefaAtualizacao = cron.schedule(intervaloAtualizacao, () => {
