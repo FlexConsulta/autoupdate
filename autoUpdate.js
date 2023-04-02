28a9ba38150c0ed43bea087037e83db330e37ddb
Anael Medeiros <anael.medeiros@CTNAPPLE71P6QCM.local>
2023-04-02 09:33:47 -0400

    add rotinas teste

---
+++ autoUpdate.js
@@ -0,0 +1,28 @@
+import cron from "node-cron";
+import { simpleGit } from "simple-git";
+
+const options = {
+  baseDir: process.cwd(),
+  binary: "git",
+  maxConcurrentProcesses: 6,
+  trimmed: false,
+};
+
+// Define o repositório e o intervalo de atualização
+const repo = simpleGit("https://github.com/anaelj/autoupdate", options);
+const intervaloAtualizacao = "*/1 * * * *"; // atualiza a cada 10 minutos
+
+// Define a tarefa de atualização
+export const tarefaAtualizacao = cron.schedule(intervaloAtualizacao, () => {
+  console.log("Verificando atualizações...");
+  repo.pull((err, atualizacao) => {
+    if (atualizacao && atualizacao.summary.changes) {
+      console.log(`Atualizado para a versão ${atualizacao.summary.changes}!`);
+    } else {
+      console.log("Nenhuma atualização disponível.");
+    }
+  });
+});
+
+// Inicia a tarefa de atualização
+// tarefaAtualizacao.start();
--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-const simpleGit = require("simple-git");
-const cron = require("node-cron");
+import { tarefaAtualizacao } from "./autoUpdate.js";
 
-// Define o repositório e o intervalo de atualização
-const repo = simpleGit("git@github.com:anaelj/autoupdate.git");
-const intervaloAtualizacao = "*/1 * * * *"; // atualiza a cada 10 minutos
-
-// Define a tarefa de atualização
-const tarefaAtualizacao = cron.schedule(intervaloAtualizacao, () => {
-  console.log("Verificando atualizações...");
-  repo.pull((err, atualizacao) => {
-    if (atualizacao && atualizacao.summary.changes) {
-      console.log(`Atualizado para a versão ${atualizacao.summary.changes}!`);
-    } else {
-      console.log("Nenhuma atualização disponível.");
-    }
-  });
-});
-
-// Inicia a tarefa de atualização
 tarefaAtualizacao.start();
